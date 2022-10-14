6deb5556e46f55f5560a9b735f534bdf3e9c0056
Lee <leewhiz119@gmail.com>
2022-10-14 10:55:39 -0400

    started server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {cors: {origin: "*"}})
+
+app.set('view engine', 'ejs')
+app.get('/home', (req, res) => {
+    res.render('home')
+})
+
+server.listen('3001', (req, res)=>{
+    console.log('server listening...')
+})
